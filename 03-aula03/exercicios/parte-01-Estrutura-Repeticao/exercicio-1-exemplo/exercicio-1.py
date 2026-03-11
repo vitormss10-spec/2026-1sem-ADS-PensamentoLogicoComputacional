a783a25116b4ec953675a99273f0d3ef3275da6f
miguel7penteado <miguel7penteado@gmail.com>
2026-03-11 13:32:02 -0300

    2026-03-11 01

---
+++ 03-aula03/exercicios/parte-01-Estrutura-Repeticao/exercicio-1-exemplo/exercicio-1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+
+frutas = ["Maçã", "Banana", "Uva"]
+for fruta in frutas:
+    print(f"Eu gosto de {fruta}")
+
---
+++ 03-aula03/exercicios/parte-02-Estrutura-Controle/exercicio-5-exemplo/exercicio-5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Passo 1: Solicita a idade ao usuário
+idade = int(input("Digite a sua idade: "))
+
+# Passo 2: Verifica a condição
+if idade >= 18:
+    print("Acesso Liberado:")
+else:
+    print("Acesso Negado !")
